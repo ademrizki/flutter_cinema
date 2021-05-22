20726dbccae9454c101f21b12c62562f5f627f1c
Ade Muhammad <ade@ina17.com>
2021-05-22 22:04:30 +0700

    Add search movie URL

--- lib/src/utils/constants/constant_url.dart
+++ lib/src/utils/constants/constant_url.dart
@@ -1,3 +1,3 @@
-const String BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const String BASE_URL = 'https://api.themoviedb.org/3/';
 const String IMAGE_BASE_URL_200 = 'https://image.tmdb.org/t/p/w200/';
 const String IMAGE_BASE_URL_400 = 'https://image.tmdb.org/t/p/w400/';
