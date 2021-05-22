a6de4d3a819fa79ffcea0b3eee4608e6e8fe3c54
Ade Muhammad <ade@ina17.com>
2021-05-22 17:16:17 +0700

    Add text theme to appBar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,9 +4,20 @@ import 'package:flutter_cinema/src/view/home_page.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _appBarTextTheme = Theme.of(context).textTheme.apply(
+          // fontFamily: 'Poppins',
+          bodyColor: Colors.black87,
+        );
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Cinema',
       theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          textTheme: _appBarTextTheme,
+        ),
+        scaffoldBackgroundColor: Colors.grey[200],
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
