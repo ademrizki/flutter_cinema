6ae1ee94fb4191c72ee2d491648710ed0c1fa6c0
Ade Muhammad <ade@ina17.com>
2021-05-22 17:25:51 +0700

    Add providers for dio package, network service injection, and movies notifier

---
+++ lib/src/provider/provider.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_cinema/src/model/movies/movies.dart';
+import 'package:flutter_cinema/src/model/state/main_state.dart';
+import 'package:flutter_cinema/src/provider/movie_list_notifier.dart';
+import 'package:flutter_cinema/src/service/network_service.dart';
+import 'package:flutter_cinema/src/utils/constants/constant_env.dart';
+import 'package:flutter_cinema/src/utils/constants/constant_url.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Dio Provider (For Fetch Data)
+final _dioProvider = Provider<Dio>(
+  (_ref) => Dio(
+    BaseOptions(
+      baseUrl: BASE_URL,
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+      queryParameters: {'api_key': MOVIE_API_KEY},
+      contentType: 'application/json',
+    ),
+  ),
+);
+
+/// Dio Provider (For Fetch Data)
+final _networkServiceProvider = Provider<NetworkService>(
+  (_ref) => NetworkService(
+    _ref.watch(_dioProvider),
+  ),
+);
+
+/// Movies Notifier Provider
+final moviesProvider =
+    StateNotifierProvider.autoDispose<MoviesNotifier, MainState<Movies>>(
+  (ref) => MoviesNotifier(
+    ref.watch(_networkServiceProvider),
+  )..fetchMovies(),
+);
