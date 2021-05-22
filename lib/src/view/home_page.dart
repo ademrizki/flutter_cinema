d5e0c4542c5d2871c71af71b795795efb638aa49
Ade Muhammad <ade@ina17.com>
2021-05-22 17:18:09 +0700

    Add search bar and grid view placeholder widget

--- lib/src/view/home_page.dart
+++ lib/src/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,9 +8,70 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Cinema'),
+        title: Row(
+          children: [
+            Icon(
+              CupertinoIcons.film,
+              color: Colors.black87,
+            ),
+            SizedBox(width: 10),
+            Text('Flutter Cinema'),
+          ],
+        ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          children: [
+            /// SEARCH FIELD
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 14,
+                  horizontal: 10,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+                prefixIcon: Icon(CupertinoIcons.search),
+              ),
+              textInputAction: TextInputAction.search,
+            ),
+            const SizedBox(height: 20),
+
+            // CupertinoSlidingSegmentedControl(children: {}, onValueChanged: (value)=>{},),
+
+            /// MOVIE LIST
+            Expanded(
+              child: GridView.builder(
+                itemCount: 8,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(bottom: 20),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) => Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
