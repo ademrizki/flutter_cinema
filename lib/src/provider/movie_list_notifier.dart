f079a63b09743cf25896a150d515a1fa7ac7cab6
Ade Muhammad <ade@ina17.com>
2021-05-22 20:43:37 +0700

    Add pagination function

--- lib/src/provider/movie_list_notifier.dart
+++ lib/src/provider/movie_list_notifier.dart
@@ -7,6 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class MoviesNotifier extends StateNotifier<MainState<Movies>> {
   MoviesNotifier(this._networkService) : super(InitialState());
 
+  final searchController = TextEditingController();
   final NetworkService _networkService;
 
   final movieSections = {
@@ -16,21 +17,54 @@ class MoviesNotifier extends StateNotifier<MainState<Movies>> {
 
   final selectedMovieSection = ValueNotifier<String>('now_playing');
 
-  Future fetchMovies() async {
+  final isLoading = ValueNotifier<bool>(false);
+
+  Movies? _movies;
+
+  int _page = 1;
+
+  bool fnOnPagination(ScrollNotification scrollInfo) {
+    if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
+      _page++;
+      fetchNextPage(page: _page);
+      return true;
+    }
+    return false;
+  }
+
+  Future fetchMovies({int? page}) async {
+    _page = 1;
     try {
       state = LoadingState();
 
       /// FETCH MOVIE LIST
-      late final _result;
+      // late final Movies _result;
       if (selectedMovieSection.value == 'now_playing') {
-        _result = await _networkService.fetchNowPlayingMovies();
+        _movies = await _networkService.fetchNowPlayingMovies();
       } else {
-        _result = await _networkService.fetchPopularMovies();
+        _movies = await _networkService.fetchPopularMovies();
       }
 
-      state = SuccessState(data: _result);
+      state = SuccessState(data: _movies!);
     } catch (e) {
       state = FailState(e.toString());
     }
+  }
+
+  Future fetchNextPage({int? page}) async {
+    late final Movies _result;
+    if (_movies != null && page != null) {
+      isLoading.value = true;
+      if (selectedMovieSection.value == 'now_playing') {
+        _result = await _networkService.fetchNowPlayingMovies(page: page);
+      } else {
+        _result = await _networkService.fetchPopularMovies(page: page);
+      }
+
+      _movies!.data!.addAll(_result.data!);
+      isLoading.value = false;
+    }
+
+    state = SuccessState(data: _movies!);
   }
 }
