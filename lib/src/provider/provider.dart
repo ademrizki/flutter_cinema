e8e03b47f34c9ef24eddceec8f5f1ababe43984b
Ade Muhammad <ade@ina17.com>
2021-05-23 12:49:15 +0700

    Add movie detail and movie cast future provider

--- lib/src/provider/provider.dart
+++ lib/src/provider/provider.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_cinema/src/model/movie_cast/movie_cast.dart';
+import 'package:flutter_cinema/src/model/movie_detail/movie_detail.dart';
 import 'package:flutter_cinema/src/model/movies/movies.dart';
 import 'package:flutter_cinema/src/model/state/main_state.dart';
+import 'package:flutter_cinema/src/provider/movie_detail_notifier.dart';
 import 'package:flutter_cinema/src/provider/movie_list_notifier.dart';
 import 'package:flutter_cinema/src/service/network_service.dart';
 import 'package:flutter_cinema/src/utils/constants/constant_env.dart';
@@ -32,4 +35,26 @@ final moviesProvider = StateNotifierProvider<MoviesNotifier, MainState<Movies>>(
   (ref) => MoviesNotifier(
     ref.read(_networkServiceProvider),
   )..fetchMovies(),
+);
+
+/// Movies Detail Notifier Provider
+final movieDetailProvider =
+    StateNotifierProvider<MovieDetailNotifier, MainState<MovieDetail>>(
+  (ref) => MovieDetailNotifier(
+    ref.read(_networkServiceProvider),
+  ),
+);
+
+/// Movie Detail Future Provider
+final movieDetailFutureProvider = FutureProvider.family<MovieDetail, int>(
+  (ref, movieId) async => await ref
+      .watch(_networkServiceProvider)
+      .fetchMovieDetail(movieId: movieId),
+);
+
+/// Movie Cast Future Provider
+final movieCastFutureProvider = FutureProvider.family<MovieCasts, int>(
+  (ref, movieId) async => await ref
+      .watch(_networkServiceProvider)
+      .fetchMovieCasts(movieId: movieId),
 );
