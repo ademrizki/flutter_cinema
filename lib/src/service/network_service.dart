6921a4998a3613bcba33852bbdb52a6bf605b384
Ade Muhammad <ade@ina17.com>
2021-05-23 12:50:39 +0700

    Implement catching error for Dio Error

--- lib/src/service/network_service.dart
+++ lib/src/service/network_service.dart
@@ -3,6 +3,7 @@ import 'package:flutter_cinema/src/model/movie_cast/movie_cast.dart';
 import 'package:flutter_cinema/src/model/movie_detail/movie_detail.dart';
 import 'package:flutter_cinema/src/model/movies/movies.dart';
 import 'package:flutter_cinema/src/service/repository.dart';
+import 'package:flutter_cinema/src/utils/exception/dio_exceptions.dart';
 
 class NetworkService implements RepositoryServices {
   NetworkService(this._dio);
@@ -11,62 +12,77 @@ class NetworkService implements RepositoryServices {
 
   @override
   Future<Movies> fetchPopularMovies({int? page = 1}) async {
-    final _data = {
-      // 'api_key': MOVIE_API_KEY,
-      'page': page,
-    };
+    try {
+      final _data = {'page': page};
 
-    final _response = await _dio.get(
-      'movie/popular',
-      queryParameters: _data,
-    );
+      final _response = await _dio.get(
+        'movie/popular',
+        queryParameters: _data,
+      );
 
-    return Movies.fromJson(_response.data);
+      return Movies.fromJson(_response.data);
+    } on DioError catch (dioError) {
+      throw DioException.fromDioError(dioError);
+    }
   }
 
   @override
   Future<Movies> fetchNowPlayingMovies({int? page = 1}) async {
-    final _data = {
-      // 'api_key': MOVIE_API_KEY,
-      'page': page,
-    };
+    try {
+      final _data = {'page': page};
 
-    final _response = await _dio.get(
-      'movie/now_playing',
-      queryParameters: _data,
-    );
+      final _response = await _dio.get(
+        'movie/now_playing',
+        queryParameters: _data,
+      );
 
-    return Movies.fromJson(_response.data);
+      return Movies.fromJson(_response.data);
+    } on DioError catch (dioError) {
+      throw DioException.fromDioError(dioError);
+    }
   }
 
   @override
-  Future<MovieDetail> fetchMovieDetail({required String movieId}) async {
-    final _response = await _dio.get('movie/$movieId');
+  Future<MovieDetail> fetchMovieDetail({required int movieId}) async {
+    try {
+      final _response = await _dio.get('movie/$movieId');
 
-    return MovieDetail.fromJson(_response.data);
+      return MovieDetail.fromJson(_response.data);
+    } on DioError catch (dioError) {
+      throw DioException.fromDioError(dioError);
+    }
   }
 
   @override
-  Future<MovieCasts> fetchMovieCasts({required String movieId}) async {
-    final _response = await _dio.get('movie/$movieId/credits');
+  Future<MovieCasts> fetchMovieCasts({required int movieId}) async {
+    try {
+      final _response = await _dio.get('movie/$movieId/credits');
 
-    return MovieCasts.fromJson(_response.data);
+      return MovieCasts.fromJson(_response.data);
+    } on DioError catch (dioError) {
+      throw DioException.fromDioError(dioError);
+    }
   }
 
   @override
-  Future<Movies> fetchMovieFromSearch(
-      {int? page = 1, required String query}) async {
-    final _data = {
-      // 'api_key': MOVIE_API_KEY,
-      'page': page,
-      'query': query,
-    };
+  Future<Movies> fetchMovieFromSearch({
+    int? page = 1,
+    required String query,
+  }) async {
+    try {
+      final _data = {
+        'page': page,
+        'query': query,
+      };
 
-    final _response = await _dio.get(
-      'search/movie',
-      queryParameters: _data,
-    );
+      final _response = await _dio.get(
+        'search/movie',
+        queryParameters: _data,
+      );
 
-    return Movies.fromJson(_response.data);
+      return Movies.fromJson(_response.data);
+    } on DioError catch (dioError) {
+      throw DioException.fromDioError(dioError);
+    }
   }
 }
