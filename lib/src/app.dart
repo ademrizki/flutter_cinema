5167b188e2d86160e8d38a547ce945f585b27b86
Ade Muhammad <ade@ina17.com>
2021-05-23 12:51:25 +0700

    Add fonts to appbar and body app, and adding route to every page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cinema/src/view/home_page.dart';
+import 'package:flutter_cinema/src/view/movie_detail_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _appBarTextTheme = Theme.of(context).textTheme.apply(
-          // fontFamily: 'Poppins',
           bodyColor: Colors.black87,
+          fontFamily: 'Nunito',
         );
 
     return MaterialApp(
       title: 'Flutter Cinema',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0,
@@ -20,7 +22,12 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey[200],
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomePage.route,
+      routes: {
+        HomePage.route: (context) => HomePage(),
+        MovieDetailPage.route: (context) => MovieDetailPage(),
+      },
     );
   }
 }
